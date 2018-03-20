d17726fa50987afd767196bb0f4ba895eb519b88
Pascal Fievet <pascalfievet@gmail.com>
2018-03-20 16:20:41 +0100

    providers geolocation

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,3 +1,6 @@
 class Provider < ApplicationRecord
   belongs_to :service
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+  validates :service_id, presence: true
 end
