df28227baf36e3f0a437ce8ec7019cebbad8333d
Pascal Fievet <pascalfievet@gmail.com>
2018-03-21 17:09:15 +0100

    autocomplete of client address

--- app/javascript/packs/autocomplete.js
+++ app/javascript/packs/autocomplete.js
@@ -1,11 +1,20 @@
 // app/javascript/components/autocomplete.js
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
-    var providerAddress = document.getElementById('provider_address');
+    var my_address = document.getElementById('provider_address');
+    var my_address2 = document.getElementById('address');
 
-    if (providerAddress) {
-      var autocomplete = new google.maps.places.Autocomplete(providerAddress, { types: [ 'geocode' ] });
-      google.maps.event.addDomListener(providerAddress, 'keydown', function(e) {
+    if (my_address) {
+      var autocomplete = new google.maps.places.Autocomplete(my_address, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(my_address, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+    if (my_address2) {
+      var autocomplete = new google.maps.places.Autocomplete(my_address2, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(my_address2, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
         }
