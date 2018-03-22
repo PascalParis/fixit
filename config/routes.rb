e88fd1cf345cead16e15ab41e9ca28648e2b4ccd
(Adela Bodea) adebodea@gmail.com <adebodea@gmail.com>
2018-03-22 15:32:42 +0100

    search, login, navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :services
   resources :providers
+  resources :profiles
+  resources :settings
 
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
