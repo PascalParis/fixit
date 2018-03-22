963a49d007d57a66199b70dd2f8ace943d3a8acc
Raoul Gauthier <raoul_gauthier@outlook.com>
2018-03-23 00:37:10 +0100

    workorder creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  get 'workorders/index'
+
+  get 'workorders/show'
+
+  get 'workorders/new'
+
+  get 'workorders/create'
+
+  get 'workorders/delete'
+
   devise_for :users
   root to: 'home#index'
   resources :services
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322141432) do
+ActiveRecord::Schema.define(version: 20180322220114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -87,4 +87,16 @@ ActiveRecord::Schema.define(version: 20180322141432) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "workorders", force: :cascade do |t|
+    t.date "date"
+    t.bigint "provider_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider_id"], name: "index_workorders_on_provider_id"
+    t.index ["user_id"], name: "index_workorders_on_user_id"
+  end
+
+  add_foreign_key "workorders", "providers"
+  add_foreign_key "workorders", "users"
 end
